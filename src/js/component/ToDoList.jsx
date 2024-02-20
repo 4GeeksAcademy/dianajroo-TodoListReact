7f88c7c77914c6cfb3885b0b7f5b453f820c947e
DianaJRoo <149723555+DianaJRoo@users.noreply.github.com>
2024-02-20 02:29:04 +0000

    ToDoList/casilisto

---
+++ src/js/component/ToDoList.jsx
@@ -0,0 +1,38 @@
+import React, {useState} from "react";
+
+
+//create your first component
+const ToDoList = () => {
+	const [task, setTask] = useState('');
+  	const [tasksList, setTasksList] = useState([]);
+
+	const handleTask = (e) => {
+		if (e.key == "Enter") {
+			setTasksList([...tasksList, 
+			{
+				label: task,
+				done: false,
+	
+			}])
+		}
+
+		setTask(e.target.value);
+	}
+
+	return (<>
+		<h1>To do List {task}</h1>
+		<input type="text" placeholder="Task" 
+		onKeyUp={ (evt)=> handleTask(evt) }/>
+
+		{tasksList.map((toDo , index)=> {
+			return <div key={index}>
+				{toDo.label}
+			</div>
+		})}
+	
+	</>
+
+	)
+};
+
+export default ToDoList;
